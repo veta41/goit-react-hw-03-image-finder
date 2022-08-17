3cfab2880a817ce216f1706c3a1769a22cf7729f
veta41 <74262484+veta41@users.noreply.github.com>
2022-08-17 22:10:25 +0300

    fix

--- src/components/Searchbar/Searchbar.styled.js
+++ src/components/Searchbar/Searchbar.styled.js
@@ -45,18 +45,6 @@ export const Button = styled.button`
   }
 `;
 
-// export const Label = styled.span`
-//   position: absolute;
-//   width: 1px;
-//   height: 1px;
-//   padding: 0;
-//   overflow: hidden;
-//   clip: rect(0, 0, 0, 0);
-//   white-space: nowrap;
-//   clip-path: inset(50%);
-//   border: 0;
-// `;
-
 export const Input = styled.input`
   display: inline-block;
   width: 100%;
